14ad8b2c3840b218ec62451e0c7c7d5ccc04d93a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-09-09 20:48:55 -0300

    applied codemod

--- src/main.go
+++ src/main.go
@@ -9,3 +9,15 @@ var errBadStuff = errors.New("something happened")
 func DoSomething() error {
 	return errors.Wrapf(errBadStuff, "some context")
 }
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+var errBadStuff = errors.New("something happened")
+
+func DoSomething() error {
+	return fmt.Errorf("some context: %w", errBadStuff)
+}
