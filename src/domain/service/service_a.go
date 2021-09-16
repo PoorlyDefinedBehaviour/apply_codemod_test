774b3667f425f9e5118053b92ce3cf2a6d60f04b
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-09-16 20:44:39 -0300

    applied codemods

--- src/domain/service/service_a.go
+++ src/domain/service/service_a.go
@@ -1,9 +1,12 @@
 package service
 
-import "github.com/pkg/errors"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
 
 var AError = errors.New("a")
 
 func DoSomethingA() error {
-	return errors.Wrapf(AError, "some context")
+	fmt.Errorf("some context: %w", AError)
 }
