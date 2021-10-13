fff08bb74e2e2ebccadb2fb6a8357bcca9604e32
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-10-13 00:06:37 -0300

    applied codemods

--- src/repositories/repo_a.go
+++ src/repositories/repo_a.go
@@ -7,21 +7,10 @@ import (
 )
 
 func Create(ctx context.Context) {
-	s := newrelic.DatastoreSegment{
-		Product:            newrelic.DatastoreMySQL,
-		Collection:         "users",
-		Operation:          "INSERT",
-		ParameterizedQuery: "INSERT INTO users (name, age) VALUES ($1, $2)",
-		QueryParameters: map[string]interface{}{
-			"name": "Dracula",
-			"age":  439,
-		},
-		Host:         "mysql-server-1",
-		PortPathOrID: "3306",
-		DatabaseName: "my_database",
-	}
+	defer StartDBSegment("users",
+		"INSERT",
+		"INSERT INTO users (name, age) VALUES ($1, $2)").End()
 
-	s = startSegment(ctx, s)
 	// ... make the datastore call
-	s.End()
+
 }
